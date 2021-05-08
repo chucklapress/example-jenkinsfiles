4a3802dfd834df394320ac0d880c966ca51734b8
chucklapress <charleslapress@gmail.com>
2021-05-07 22:58:49 -0400

    added robot test shell script

---
+++ robot_tests.sh
@@ -0,0 +1,2 @@
+pip3 install robotframework robotframework-seleniumlibrary
+python3 -m robot.run --outputdir reports /var/lib/jenkins/workspace/robot-test /var/lib/jenkins/workspace/robot-test/resource/tests/*.robot 
