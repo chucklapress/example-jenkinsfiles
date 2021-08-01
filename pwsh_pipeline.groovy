aec9daeb4fa784b52e3c05edd251c5ba09542e83
chucklapress <charleslapress@gmail.com>
2021-08-01 01:36:59 -0400

    added slack notifications and clean workspace to Jenkinsfile

--- pwsh_pipeline.groovy
+++ pwsh_pipeline.groovy
@@ -1,13 +1,40 @@
+def notify(status) {
+    slackSend channel: "#general",
+    color: '#1f29d7',
+    message: "${status}",
+    tokenCredentialId: 'slack-token'
+}
 pipeline {
   agent any
+
   stages {
+      stage('Slack Notify kick off') {
+          steps {
+              notify('pwsh-pipeline kicking off')
+          }
+      }
       stage('Stage 1') {
           steps {
+          notify("Build Started")
           pwsh ('''
                     $text = ((Invoke-Webrequest -Uri "https://uselessfacts.jsph.pl/random.json?language=en" -UseBasicParsing).content | ConvertFrom-Json).text
                     Write-Output "$text"
             ''')
           }
       }
+      stage('Stage 2') {
+          steps {
+              pwsh ('''
+                    $details = ($PSVersionTable.PSVersion)
+                    Write-Output "Here are the details of the powershell version" "`n $details"
+              ''')
+              notify("Build Completed")
+          }
+      }
   }
+  post {
+        always {
+            cleanWs()
+        }
+    }
 }
